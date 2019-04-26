fa2012aabcc2b509e927926abea2ae8d9ede261c
Lauren Hanson <laurenhanson@laurens-macbook.attlocal.net>
2019-04-25 20:43:06 -0500

    add rules and accounts pages again

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="header">
+        Audit Cloud
+    </div>
+);
+
+export default Header;
--- src/pages/rules.js
+++ src/pages/rules.js
@@ -7,6 +7,5 @@ export default () => (
     <Header />
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 )
