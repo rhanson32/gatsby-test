d12327b224bdbbc15390f9ed6907f871bbf3544e
Lauren Hanson <laurenhanson@laurens-macbook.attlocal.net>
2019-04-25 20:45:38 -0500

    adding account page text

---
+++ src/pages/accounts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from '../components/Header';
+
+export default () => (
+  <div className="app">
+    <Header />
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+  </div>
+)
