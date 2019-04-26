d324f07dfd01368291ada7affed935dbdbb0b4ad
Lauren Hanson <laurenhanson@laurens-macbook.attlocal.net>
2019-04-25 21:59:58 -0500

    test

--- src/pages/accounts.js
+++ src/pages/accounts.js
@@ -1,11 +1,53 @@
 import React from "react";
-
+import AccountItem from '../components/AccountItem';
 import Header from '../components/Header';
 
-export default () => (
-  <div className="app">
-    <Header />
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
-  </div>
-)
+class Accounts extends React.Component {
+
+    state = {
+        Accounts: [ 
+            {
+                Id: '12345',
+                Provider: 'AWS',
+                RoleName: 'testRole1'
+            },
+            {
+                Id: '12456',
+                Provider: 'AWS',
+                RoleName: 'testRole2'
+            },
+            {
+                Id: '23456',
+                Provider: 'AWS',
+                RoleName: 'testRole3'
+            },
+            {
+                Id: '56789',
+                Provider: 'AWS',
+                RoleName: 'testRole4'
+            }
+        ]
+    };
+
+    render() {
+        console.log(this.state);
+        return (
+            <div className="app">
+                <Header />
+                <div className="accounts">
+                    <div className="account-list">
+                    {
+                        this.state.Accounts.map(account => {
+                            return (
+                                <AccountItem key={account.Id} item={account}/>
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Accounts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-
 import Header from '../components/Header';
 
 export default () => (
   <div className="app">
     <Header />
-    <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
