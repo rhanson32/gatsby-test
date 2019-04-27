f20ddd8afb3aefbc846c2509fe632d5de2d2a6ce
Lauren Hanson <laurenhanson@laurens-macbook.attlocal.net>
2019-04-26 22:01:00 -0500

    first api call again

--- src/components/RuleItem.js
+++ src/components/RuleItem.js
@@ -7,7 +7,7 @@ class RuleItem extends React.Component {
     toggleRule = () => {
         console.log(this.props);
 
-        axios.get('https://youthful-jones-9692d2.netlify.com/.netlift/functions/hello.js').then(
+        axios.get('https://youthful-jones-9692d2.netlify.com/.netlify/functions/hello.js').then(
             response => console.log(response)
         ).catch(err => console.log(err));
         // this.props.toggleRule(this.props.rule.Description);
