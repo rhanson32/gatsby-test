dffb47aec2b0f611096d1862fcdca6e8ec0fe1d4
Lauren Hanson <laurenhanson@laurens-macbook.attlocal.net>
2019-04-26 22:25:40 -0500

    added features page

---
+++ src/components/ExternalHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+const ExternalHeader = () => (
+    <div className="header">
+        <div className="header-title">
+            Audit Cloud
+        </div>
+        <div className="header-menu">
+            <Link activeClassName="active-link" to="/features/">Features</Link>
+            <Link activeClassName="active-link" to="/">Home</Link>
+            <Link activeClassName="active-link" to="/accounts/">Accounts</Link>
+        </div>  
+    </div>
+);
+
+export default ExternalHeader;
---
+++ src/pages/features.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ExternalHeader from '../components/ExternalHeader';
+
+const Features = () => (
+    <div>
+        <ExternalHeader />
+        Features!
+    </div>
+);
+
+export default Features;
