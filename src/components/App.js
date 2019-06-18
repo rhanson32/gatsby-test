e8af90dd7115bcba589a5dd120b6abd319ab495f
unknown <reed.hanson@gmail.com>
2019-06-17 22:12:09 -0500

    added History page to App.js again

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import Pricing from '../components/Pricing'
 import SupportPage from '../components/SupportPage'
 import Settings from '../components/Settings'
 import Docs from '../components/Docs';
+import History from '../components/History';
 
 import store from '../store'
 
