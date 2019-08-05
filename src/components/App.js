bbd3ca20aba6823bfbae4b112f89526e929f6dc4
unknown <reed.hanson@gmail.com>
2019-08-04 22:43:48 -0500

    saml page 3

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import SupportPage from '../components/SupportPage'
 import Settings from '../components/Settings'
 import Docs from '../components/Docs';
 import History from '../components/History';
-import SAML from './SAML';
+import SAML from '../components/SAML';
 
 import store from '../store'
 
