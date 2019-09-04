9f1f61be261b0a1e223826fabbd7f5a9b45f7a99
unknown <reed.hanson@gmail.com>
2019-09-04 12:51:48 -0500

    add check for window for compose function

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-
 import reducer from '../reducers';
-console.log("Node version:",$NODE_VERSION);
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const composeEnhancers = $COMPOSE || window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const composeEnhancers = window ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+// const composeEnhancers = compose;
 
 const store = createStore(
     reducer,
