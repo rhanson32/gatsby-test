b51669a5deac66e177de5a4dd8a18e92a897439c
unknown <reed.hanson@gmail.com>
2019-09-04 12:26:27 -0500

    store update to use environment variable for compose take 2

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,9 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducer from '../reducers';
-
+console.log("Node version:",$NODE_VERSION);
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const composeEnhancers = process.env.COMPOSE || window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = $COMPOSE || window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 const store = createStore(
     reducer,
