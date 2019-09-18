ac5b3b665c13733a045e4f161e49809f050ef9e0
unknown <reed.hanson@gmail.com>
2019-09-17 21:13:51 -0500

    clean up unneeded code in store file

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,7 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from '../reducers';
 
-console.log(process.env.NODE_ENV);
-
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
-// const composeEnhancers = devCompose || compose;
 
 const store = createStore(
     reducer,
